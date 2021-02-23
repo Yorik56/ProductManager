7897e99baf4168a0c896ec9a48f20a62d110e274
moreau <yorik-ba-56@hotmail.Fr>
2021-02-24 00:01:38 +0100

    first commit

---
+++ src/fr/mds/productmanager/servlet/LogoutServlet.java
@@ -0,0 +1,26 @@
+package fr.mds.productmanager.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns="/logout")
+public class LogoutServlet  extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getSession().removeAttribute("username");
+		resp.sendRedirect(req.getContextPath() + "/login.html");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			doGet(req, resp);
+	}
+	
+}
